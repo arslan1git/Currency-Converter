baed545fefe4762198078a7d6b7129adfe64a192
arslan1git <arslanalichanna1@gmail.com>
2024-04-21 11:28:40 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    PKR: 278.12,
+    EUR: 0.92,
+    UAE: 3.67,
+    INR: 83.33,
+};
+let user = await inquirer.prompt([
+    {
+        name: "from",
+        type: "list",
+        message: "Enter from Currency",
+        choices: ["USD", "PKR", "EUR", "UAE", "INR"],
+    },
+    {
+        name: "to",
+        type: "list",
+        message: "Enter to Currency",
+        choices: ["USD", "PKR", "EUR", "UAE", "INR"],
+    },
+    {
+        name: "amount",
+        type: "number",
+        message: "Enter your amount",
+    },
+]);
+let fromAmount = currency[user.from];
+let toAmount = currency[user.to];
+let amount = user.amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
